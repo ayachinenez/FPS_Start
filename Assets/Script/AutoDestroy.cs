cd5208b03ce1dde75668ad5608974922d219c6f0
杏子天下第一 <50146098+ayachinenez@users.noreply.github.com>
2020-07-08 14:34:48 +0800

    Initial commit

---
+++ Assets/Script/AutoDestroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    public float m_timer = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(this.gameObject, m_timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
